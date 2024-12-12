f2c20468decb4826cdbb45b09eb592daf8ca51bf
Joshua McNeel <joshmcneel@gmail.com>
2024-12-12 13:29:40 -0500

    chore: more intelligent comments

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,9 +1,13 @@
 class SudokuSolver {
   validate(puzzleString) {
+    // Early return if puzzle string isn't the right length
     if (puzzleString.length !== 81) {
       return false;
     }
+
+    // Store regex valid characters in a variable to,
     const validChars = /^[1-9.]+$/;
+    // use test() to validate the whole puzzle.
     return validChars.test(puzzleString);
   }
 
@@ -12,7 +16,9 @@ class SudokuSolver {
     const startIndex = rowIndex * 9;
     const endIndex = startIndex + 9;
     
+    // Check the entire row for conflicts
     for (let i = startIndex; i < endIndex; i++) {
+      // Ignore the current cell inside this row
       if (puzzleString[i] === value.toString()) {
         return false;
       }
@@ -23,10 +29,11 @@ class SudokuSolver {
   checkColPlacement(puzzleString, row, column, value) {
     const colIndex = column - 1;
     
-    // Check each row at this column index
+    // Check the entire column for conflicts
     for (let i = 0; i < 9; i++) {
       const index = (i * 9) + colIndex;
 
+      // Ignore the current cell inside this column
       if (puzzleString[index] === value.toString()) {
         return false;
       }
@@ -49,6 +56,7 @@ class SudokuSolver {
         const currentCol = boxColStart + j;
         const index = (currentRow * 9) + currentCol;
         
+        // Ignore the current cell in this region
         if (puzzleString[index] === value.toString()) {
           return false;
         }
@@ -58,19 +66,26 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
+    // Early return if the puzzle is invalid
     if (!this.validate(puzzleString)) {
       return false;
     }
 
+    // Create an array out of the string for processing
     let puzzle = puzzleString.split('');
+
+    // Return the puzzle as a string
     if (this.solveSudoku(puzzle)) {
       return puzzle.join('');
     }
+
     return false;
   }
 
   solveSudoku(puzzle) {
     const emptyCell = puzzle.indexOf('.');
+
+    // Early return if the puzzle has been solved
     if (emptyCell === -1) {
       return true; // Puzzle is solved
     }
